a012ff6693e7c7acbd3e578ba32149ca951ed96c
Marcelo Andrade <marcelo.dasilvaandrade@rpn.ch>
2017-11-17 16:26:49 +0100

    Create an initial project

---
+++ WpfCalculette/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCalculette
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            double dblnb1 = Convert.ToInt32(tbxNb1.Text);
+            double dblnb2 = Convert.ToInt32(tbxNb2.Text);
+            double dblResponse = dblnb1 + dblnb2;
+            lblResponse.Content = dblResponse.ToString();
+        }
+    }
+}
